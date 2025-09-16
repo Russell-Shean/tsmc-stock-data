7a817a419be681688833f0a33a96ab66ea6081e8
Russell-Shean <russshean@gmail.com>
2025-09-16 16:04:52 +0800

    reconcile merge conflicts

--- get_finmind_data.py
+++ get_finmind_data.py
@@ -32,19 +32,16 @@ tsm_daily_close  = api.taiwan_stock_daily(
 
 tsm_daily_close.to_csv('data/tsmc/daily_closing_price.csv', index=False)
 
-<<<<<<< HEAD
-# Download daily closing price data ----------------------------------------------------------------------------------------------
-#tsm_daily_close  = api.taiwan_stock_monthly(
+# Download monthly closing price data ----------------------------------------------------------------------------------------------
+#tsm_monthly_close  = api.taiwan_stock_monthly(
   #                stock_id=stock_id,
   #                 start_date=start_date,
  #                  end_date=end_date
 #)
 
 
-#tsm_daily_close.to_csv('data/tsmc/monthly_closing_price.csv', index=False)
+#tsm_monthly_close.to_csv('data/tsmc/monthly_closing_price.csv', index=False)
 
-=======
->>>>>>> 5258ae6610c899a37812cc91de33ed50e27c76a9
 
 # Monthly revenue -----------------------------------------------------------------------------------------------------------------------
 #tsm_monthly_revenue = api.taiwan_stock_month_revenue(
@@ -71,7 +68,6 @@ institutional_buy_and_sell = api.taiwan_stock_institutional_investors(
 institutional_buy_and_sell.to_csv('data/tsmc/tsmc_institutional_buy_and_sell.csv', index=False)
 
 
-<<<<<<< HEAD
 # 台股八大行庫賣賣表	TaiwanstockGovernmentBankBuySell
 
 #taiwan_stock_government_bank_buy_sell = api.taiwan_stock_government_bank_buy_sell(
@@ -99,7 +95,6 @@ institutional_buy_and_sell.to_csv('data/tsmc/tsmc_institutional_buy_and_sell.csv
 #)
 
 #taiwan_futures_open_interest_large_traders.to_csv('data/tsmc/taiwan_futures_open_interest_large_traders.csv', index=False)
-=======
 
 time.sleep(1) 
 # 外資持股表 	Taiwan Stock Shareholding -----------------------------------------
@@ -168,4 +163,3 @@ taiwan_stock_weekly = api.taiwan_stock_monthly(
 
 
 )
->>>>>>> 5258ae6610c899a37812cc91de33ed50e27c76a9
