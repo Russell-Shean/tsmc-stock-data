ba47b0cd467dbfacad0e0e1e4bf5bb0150632847
Russ <russshean@gmail.com>
2025-10-03 17:07:48 +0800

    update table

---
+++ create_md_table.R
@@ -0,0 +1,13 @@
+data_files <- list.files("data/tsmc") 
+
+  
+"https://github.com/Russell-Shean/tsmc-stock-data/blob/main/data/tsmc/GDP.csv"
+
+
+paste0("| ",
+       1:length(data_files), 
+       ". [", 
+       data_files,
+       "](https://github.com/Russell-Shean/tsmc-stock-data/blob/main/data/tsmc/", 
+       data_files,
+       ") | -  | -  | -  |\n") |> cat()
