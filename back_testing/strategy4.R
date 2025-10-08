6708afc4740bdfb619476723800cca287ac5206f
Russell-Shean <russshean@gmail.com>
2025-10-08 18:50:07 +0800

    add strategy 1 data

--- back_testing/strategy4.R
+++ back_testing/strategy4.R
@@ -8,5 +8,8 @@ tsmc_daily_closing |> mutate(alltime_max = cummax(close),
 
 tsmc_daily_closing |> 
   ggplot2::ggplot(aes(x=date, y=close, group = 1)) +
-  geom_line(col= "blue") #+ 
+  geom_line(col= "blue") +
  # geom_point(alpha = 0.3) 
+  labs(title = "Daily Closing Price of TSMC - 2016 to Today",
+       x = "Date",
+       Y = "Daily Closing Price")
